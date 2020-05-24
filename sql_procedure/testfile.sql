3739fd82e51753e5a631ff7f3abe86dd6a81fd21
lalisembs <65862448+lalisembs@users.noreply.github.com>
2020-05-24 12:35:38 -0400

    added email

---
+++ sql_procedure/testfile.sql
@@ -0,0 +1,7 @@
+Exec msdb.dbo.sp_send_dbmail
+
+@profile_name = 'Tsega_Profile',
+@recipients = 'tsega492@gmail.com',
+@body = 'The store procedure finished, thanks' ,
+@subject = 'Email Deliver' 
+
